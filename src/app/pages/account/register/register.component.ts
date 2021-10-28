a6b1996998b22523eead6f917960600e9807cef7
Ritesh Das <ritesh.d@quodisys.com>
2021-10-28 18:05:18 +0530

    Signup form implement FE

--- src/app/pages/account/register/register.component.ts
+++ src/app/pages/account/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl} from '@angular/forms';
 
+// @ts-check
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -9,16 +10,39 @@ import { FormGroup, FormBuilder, Validators} from '@angular/forms';
 export class RegisterComponent implements OnInit {
 
   form: FormGroup;
+  submitted = false;
   constructor(private fromBuilder: FormBuilder) { }
   
   ngOnInit(): void {
-      this.fromBuilder.group({
-        fname: [null, [Validators.required]],
-        lname: [null, [Validators.required]],
-        email: [null, [Validators.required, Validators.email]],
-        password: [null, [Validators.required, Validators.minLength(8), Validators.maxLength(16)]],
-        repeat_password: [null, [Validators.required]]
-      });
+     this.form =  new FormGroup({
+        'fname': new FormControl(null, [Validators.required]),
+        'lname': new FormControl(null, [Validators.required]),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'password': new FormControl(null, [Validators.required, Validators.minLength(8), Validators.maxLength(16)]),
+        'repeat_password': new FormControl(null, [Validators.required]),
+        'phone': new FormControl(null, [Validators.required, Validators.pattern('[0-9]*')])
+      },
+      /*{
+        validators: [Validation.match('password', 'confirmPassword')]
+      }*/);
+  }
+
+  get fname() { return this.form.get('fname'); }
+  get lname() { return this.form.get('lname');}
+  get email() { return this.form.get('email'); }
+  get password() { return this.form.get('password');}
+  get repeat_password() { return this.form.get('repeat_password'); }
+  get phone() { return this.form.get('phone');}
+
+  onSubmit(): void {
+    alert('Clicked!');
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    console.log(JSON.stringify(this.form.value, null, 2));
   }
 
 }
